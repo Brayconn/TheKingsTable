95d94219d29ff51c4cdfc0f4101d911eb23c35af
Brayconn <29030756-Brayconn@users.noreply.github.com>
2021-04-17 17:48:01 -0700

    Updated Attributes so they load more correctly

--- CaveStoryModdingFramework/Attributes.cs
+++ CaveStoryModdingFramework/Attributes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using PixelModdingFramework;
 
@@ -16,11 +17,20 @@ namespace CaveStoryModdingFramework.Maps
         {
             Tiles = new List<byte>(new byte[Width * Height]);
         }
-        public Attribute(string path)
+        public Attribute(string path) : this()
         {
-            if(new FileInfo(path).Length != Width * Height)
-                throw new FileLoadException(); //TODO text, or myabe don't throw?
-            Tiles = new List<byte>(File.ReadAllBytes(path));
+            Load(path);
+        }
+        public void Load(string path)
+        {
+            var tiles = File.ReadAllBytes(path);
+            for (int i = 0; i < tiles.Length; i++)
+                Tiles[i] = tiles[i];
+        }
+        public void Clear(byte val = 0)
+        {
+            for (int i = 0; i < Width * Height; i++)
+                Tiles[i] = val;
         }
 
         public void Save(string path)
